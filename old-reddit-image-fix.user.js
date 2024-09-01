c3d47fbcd6171f5a8d203bafe240dcedaee8f5a7
Fruitsnack01 <92564151+Fruitsnack01@users.noreply.github.com>
2024-08-31 23:26:51 -0400

    Update and rename old-reddit-image-comments.user.js to old-reddit-image-fix.user.js
    fixes images in original posts

--- old-reddit-image-fix.user.js
+++ old-reddit-image-fix.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         Old Reddit Image Comments
+// @name         Old Reddit Image Fix
 // @namespace    http://tampermonkey.net/
-// @version      1.0
-// @description  Displays images in comments on the old Reddit website
+// @version      1.1
+// @description  Fixes images in comments and posts on the old Reddit website
 // @author       fruitsnack01
 // @match        *://www.reddit.com/*
 // @match        *://old.reddit.com/*
@@ -29,6 +29,23 @@
             img.addEventListener('click', function() {
                 const img_src = img.src;
                 window.open(img_src, '_blank');
+            });
+        }
+
+        if (element && element.textContent.includes('https://preview.redd.it/')) {
+            const img2 = document.createElement('img');
+            img2.src = element.href;
+            //img2.style.width = '240px';
+            img2.style.maxWidth = '100%';
+            element.replaceWith(img2);
+
+            img2.addEventListener('mouseover', function() {
+                img2.style.cursor = 'pointer';
+            });
+
+            img2.addEventListener('click', function() {
+                const img2_src = img2.src;
+                window.open(img2_src, '_blank');
             });
         }
     });
